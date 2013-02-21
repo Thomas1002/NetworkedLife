c200203e68e798f09009e1eeaa7a260ba8cb0978
Thomas Andersen <taprivat@gmail.com>
2013-02-21 21:36:18 +0100

    commit

--- Network20q/src/chapter1/program.py
+++ Network20q/src/chapter1/program.py
@@ -1,11 +1,10 @@
-
-import sys
-from plotter import *
+import calc as ca
+import plotter as p
 
 class Program:
   def run(self, channelGains, power, noise, gamma):
     calc = ca.Calc(channelGains, noise, gamma)
     result = calc.result(power, 10)
-    plotter = Plotter()
+    plotter = p.Plotter()
     plotter.plot(result[0], 'mW', 0, 2)
     plotter.plot(result[1], 'SIR', 0.5, 3)
